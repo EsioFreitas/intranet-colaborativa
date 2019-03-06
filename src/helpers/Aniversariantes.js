4b3df44d94454613f4ca2eec381d54c3de84e3b1
EsioFreitas <esio.gustavo@gmail.com>
2019-03-06 17:03:21 -0300

    Add aniversariantes helper

---
+++ src/helpers/Aniversariantes.js
@@ -0,0 +1,10 @@
+import PedroPhoto from '../assets/images/perfil2.jpg'
+
+const aniversariantes ={
+    pedro: {
+        name: "Pedro Carlos",
+        data: "28/09",
+        photo: PedroPhoto
+    }
+}
+export default aniversariantes;
