5f21b4e1773cdecbf049a1fdbe9927ca556c955c
EsioFreitas <esio.gustavo@gmail.com>
2019-03-06 17:34:07 -0300

    Add aniversariantes component

--- src/components/Aniversariantes/Aniversariantes.js
+++ src/components/Aniversariantes/Aniversariantes.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
+import aniverHelper from '../../helpers/Aniversariantes'
+
 import './Aniversariantes.scss';
 
-const aniversariantes = props =>(
-    <div>
+const aniversariantes = props => (
+    <div className='aniversariantes'>
         <h1>Aniversariantes</h1>
-        <div>
-            <div>
-                <img/>
-                <h3></h3>
-                <p></p>
-            </div>
+        <div className='aniversariantes__carousel'>
+            {
+                Object.keys(aniverHelper).map(pessoa => (
+                    <div key={pessoa} className='aniversariantes__pessoa'>
+                        {console.log(aniverHelper[pessoa].photo, aniverHelper[pessoa])}
+                        <img src={aniverHelper[pessoa].photo} alt={aniverHelper[pessoa].name} />
+                        <div>
+                            <h3>{aniverHelper[pessoa].name}</h3>
+                            <p>{aniverHelper[pessoa].data}</p>
+                        </div>
+                    </div>
+                ))
+            }
 
-            <div></div>
-            <div></div>
+
+            <div className='aniversariantes__carousel__controuls-btn aniversariantes__carousel__controuls-btn--prev'>&#10094;</div>
+            <div className='aniversariantes__carousel__controuls-btn aniversariantes__carousel__controuls-btn--next'>&#10095;</div>
         </div>
     </div>
 );
