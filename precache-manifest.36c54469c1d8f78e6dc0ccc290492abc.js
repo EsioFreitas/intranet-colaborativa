eb0b4168bbdec39c48f8961fb5922010c256efb5
EsioFreitas <esio.gustavo@gmail.com>
2019-03-07 12:00:32 -0300

    Updates

---
+++ precache-manifest.36c54469c1d8f78e6dc0ccc290492abc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d271add14625f7cb7e42",
+    "url": "/intranet-colaborativa/static/css/main.0b6ebbc7.chunk.css"
+  },
+  {
+    "revision": "d271add14625f7cb7e42",
+    "url": "/intranet-colaborativa/static/js/main.d271add1.chunk.js"
+  },
+  {
+    "revision": "784d45c4b08e113220cd",
+    "url": "/intranet-colaborativa/static/js/runtime~main.784d45c4.js"
+  },
+  {
+    "revision": "165e603e5317eee103b1",
+    "url": "/intranet-colaborativa/static/js/2.165e603e.chunk.js"
+  },
+  {
+    "revision": "9967d91861b80475092ad40323351240",
+    "url": "/intranet-colaborativa/static/media/perfil.9967d918.jpg"
+  },
+  {
+    "revision": "6f71cfa78c03204ce3ee7b86ce29d3ae",
+    "url": "/intranet-colaborativa/static/media/logo.6f71cfa7.svg"
+  },
+  {
+    "revision": "2299b9639a9963ec63e7778d336997ab",
+    "url": "/intranet-colaborativa/static/media/equipe.2299b963.jpg"
+  },
+  {
+    "revision": "441bda1519c93a9c5cc3389bec607dce",
+    "url": "/intranet-colaborativa/static/media/sicoob.441bda15.jpg"
+  },
+  {
+    "revision": "dddaac7b86e4ee3dee9ab67d123b841e",
+    "url": "/intranet-colaborativa/static/media/estudos.dddaac7b.jpeg"
+  },
+  {
+    "revision": "9ef392e39aa7e0202319a9a05edb22c8",
+    "url": "/intranet-colaborativa/static/media/jogos.9ef392e3.jpg"
+  },
+  {
+    "revision": "5b8c2afd68cd6c476e63dbb035bfb418",
+    "url": "/intranet-colaborativa/static/media/perfil2.5b8c2afd.jpg"
+  },
+  {
+    "revision": "ee9e8f39c18c1623027c687a24a930ae",
+    "url": "/intranet-colaborativa/static/media/perfil3.ee9e8f39.jpg"
+  },
+  {
+    "revision": "675386fefbb7725fa96f6fc08f3de9e8",
+    "url": "/intranet-colaborativa/static/media/perfil5.675386fe.jpg"
+  },
+  {
+    "revision": "28dbaa9d1e9f75faa139e864f22c4d60",
+    "url": "/intranet-colaborativa/static/media/perfil4.28dbaa9d.jpg"
+  },
+  {
+    "revision": "402dfa12f99a80200632551cd743d7f0",
+    "url": "/intranet-colaborativa/static/media/reuniao.402dfa12.jpg"
+  },
+  {
+    "revision": "f2388c8858327e21b0d213c837d22862",
+    "url": "/intranet-colaborativa/static/media/backgraund.f2388c88.svg"
+  },
+  {
+    "revision": "7330beeb90d355a2cee6214f8d3cccf8",
+    "url": "/intranet-colaborativa/index.html"
+  }
+];
